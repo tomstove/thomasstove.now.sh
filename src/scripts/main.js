bc97dc649e54609dd50f932977706e8648c3cab4
Tom Stove <thomstove@gmail.com>
2018-01-13 13:04:59 +1100

    Added more content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,12 +26,46 @@ function hideSheet(el) {
 
 var overlay = document.getElementById("overlay");
 
+// open selectedWorksSheet
 document.getElementById('selectedWorks').addEventListener('click', function(e) {
-	console.log(e);
+	//console.log(e);
 	showSheet('selectedWorksSheet');
 });
 
-document.getElementById('overlay').addEventListener('click', function(e) {
-	console.log(e);
+// open sketchbookSheet
+document.getElementById('sketchbook').addEventListener('click', function(e) {
+	//console.log(e);
+	showSheet('sketchbookSheet');
+});
+
+// open bioSheet
+document.getElementById('bio').addEventListener('click', function(e) {
+	//console.log(e);
+	showSheet('bioSheet');
+});
+
+// close selectedWorksSheet when worksCloseBtn is clicked
+document.getElementById('worksCloseBtn').addEventListener('click', function(e) {
+	//console.log(e);
 	hideSheet('selectedWorksSheet');
+});
+
+// close sketchbookSheet when sketchbookCloseBtn is clicked
+document.getElementById('sketchbookCloseBtn').addEventListener('click', function(e) {
+	//console.log(e);
+	hideSheet('sketchbookSheet');
+});
+
+// close bioSheet when bioCloseBtn is clicked
+document.getElementById('bioCloseBtn').addEventListener('click', function(e) {
+	//console.log(e);
+	hideSheet('bioSheet');
+});
+
+// close sheet when overlay is clicked
+document.getElementById('overlay').addEventListener('click', function(e) {
+	//console.log(e);
+	hideSheet('selectedWorksSheet');
+	hideSheet('sketchbookSheet');
+	hideSheet('bioSheet');
 });
